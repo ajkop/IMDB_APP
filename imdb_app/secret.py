99c5c76c02a46a9f5be1498df3e0107f31ffc928
ajkop <aj.kopczynski@gmail.com>
2019-02-26 03:29:49 -0600

    Removing old secret file

--- imdb_app/secret.py
+++
@@ -1,7 +0,0 @@
-MYSQL_USER = 'imdb_sqlalc'
-MYSQL_PASS = 'OmErmiNDefac'
-MYSQL_HOST = 'localhost'
-MYSQL_DB = 'einventory'
-
-
-SQLALCHEMY_DATABASE_URI = f'mysql://{MYSQL_USER}:{MYSQL_PASS}@{MYSQL_HOST}/{MYSQL_DB}'
