4ed59cc39ba7ff5b942c8ec9eb766b93b93f888b
ajkop <aj.kopczynski@gmail.com>
2019-02-27 12:30:00 -0600

    Removing testing changes.

--- imdb_app/config/__init__.py
+++ imdb_app/config/__init__.py
@@ -3,10 +3,9 @@ class BaseConfig(object):
     TESTING = False
     JSONIFY_PRETTYPRINT_REGULAR = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'mysql://imdb_sqlalc:OmErmiNDefac@localhost/einventory'
 
     IMDB_DOWNLOAD_URL = 'https://datasets.imdbws.com'
-    IMDB_DATA_DOWNLOAD_DIR = '/home/aj/Downloads/imdb-data'
+    IMDB_DATA_DOWNLOAD_DIR = '/root/imdb-data'
     IMDB_TITLES_FILE = f'{IMDB_DATA_DOWNLOAD_DIR}/title.basics.tsv'
     IMDB_NAMES_FILE = f'{IMDB_DATA_DOWNLOAD_DIR}/name.basics.tsv'
     IMDB_RATINGS_FILE = f'{IMDB_DATA_DOWNLOAD_DIR}/title.ratings.tsv'
